6247d4307b9c5d4da09578ace2a2c303abad8972
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2022-02-05 00:39:12 -0300

    First commit

---
+++ src/main/java/com/envioemail/zup/envioemailpoc/EnvioEmailPocApplication.java
@@ -0,0 +1,13 @@
+package com.envioemail.zup.envioemailpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioEmailPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioEmailPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/envioemail/zup/envioemailpoc/EnvioEmailPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envioemail.zup.envioemailpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioEmailPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
